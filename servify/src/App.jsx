057149a28d080049af42f970ba7a00101923a868
Kotapatipavansai <pavansai779977@gmail.com>
2026-04-18 15:19:31 +0530

    booking setup work

--- servify/src/App.jsx
+++ servify/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage   from "./pages/LandingPage";
-import ServicesPage  from "./pages/ServicePage";
+import LandingPage  from "./pages/LandingPage";
+import ServicesPage from "./pages/ServicePage";
+import BookingPage  from "./pages/BookingPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"        element={<LandingPage />} />
-        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/"          element={<LandingPage />}  />
+        <Route path="/services"  element={<ServicesPage />} />
+        <Route path="/book/:id"  element={<BookingPage />}  />
       </Routes>
     </BrowserRouter>
   );
